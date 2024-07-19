6dfe97ce674ca9d83da3e3ce326524533dbc806c
mylvep <mylvepmy@naver.com>
2024-07-19 14:55:56 +0900

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,16 @@
+//window.alert("오늘 자바 스크립트 배우는 날")
+//window.alert("주먹밥");
+//window.console.log("주말에는 놀러갈거임");
+//주말계획:
+//오늘 저녁메뉴:
+//자바스크립트 후기:
+
+//window.console.log("영화보러감");
+//window.console.log("쌀국수");
+//window.console.log("어렵겠지만 화이팅");
+
+const a =1;
+const b="아메리카노";
+const c="메가커피";
+const d=window.prompt("너의 최애 커피는?");
+window.alert(d);
---
+++ index.js
@@ -0,0 +1,2 @@
+//window.alert("자바스크립트 세팅 완료")
+window.prompt9("너의 최애커피는")
