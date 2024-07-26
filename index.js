eaac3b92bc1524f9773ee49d7fede13c77fffec4
mylvep <mylvepmy@naver.com>
2024-07-26 14:48:06 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-//window.alert("자바스크립트 세팅 완료")
-window.prompt9("너의 최애커피는")
+function oliceyoung(price){
+    if(price >=5000){
+        return price *0.8;
+    }else if(price >=3000){
+        return price *0.9
+    }else{
+        return price
+    }
+}
+
+//"coffee".toUpperCase()
